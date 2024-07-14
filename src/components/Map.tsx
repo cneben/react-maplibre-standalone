6993e4c10ab74a9672c05176ac07f4570dede559
cneben <benoit@destrat.io>
2024-07-14 23:41:56 +0200

    #5 Tentative sky implementation, map is not loaded. WIP.

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,20 +1,65 @@
 /** @jsxImportSource @emotion/react */
 import { useRef, useEffect, useState } from "react";
-import maplibregl from "maplibre-gl";
+import maplibregl, {MapEventType} from "maplibre-gl";
 import "maplibre-gl/dist/maplibre-gl.css";
+import { StyleSpecification, SourceSpecification, VectorSourceSpecification } from '@maplibre/maplibre-gl-style-spec';
 
-const Map = () => {
+const Map: React.FC = () => {
   const mapContainer = useRef<HTMLDivElement | null>(null);
+  const [style, setStyle] = useState<StyleSpecification | null>(null);
   const map = useRef<maplibregl.Map | null>(null);
   const [lat] = useState(45.92);
   const [lng] = useState(6.87);
   const [zoom] = useState(14);
 
   useEffect(() => {
+    if (style !== null) return;
+    const baseUrl = `${window.location.origin}${import.meta.env.BASE_URL}`;
+    const styleUrl = `${baseUrl}style.json`;
+    console.error("baseUrl=" + baseUrl);
+    console.error("styleUrl=" + styleUrl);
+
+    // Note 20240714: Manually modify base url for ressources loaded
+    // in style.json to fit the current Vite.js deployment.
+    async function initStyle(url: string) {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`Failed to fetch style JSON: ${response.statusText}`);
+      }
+      let refStyle = (await response.json()) as StyleSpecification;
+      if (refStyle && refStyle.sources) {
+        Object.values(refStyle.sources).forEach(
+          (source: SourceSpecification) => {
+            let tiles = (source as VectorSourceSpecification).tiles;
+            if (tiles) {
+                (source as VectorSourceSpecification).tiles = tiles.map<string>((tile) : string => {
+                return tile.replace("{baseUrl}", baseUrl);
+              });
+            }
+          }
+        );
+      }
+      if (refStyle && refStyle.sprite) {
+        // @ts-expect-error
+        refStyle.sprite = refStyle.sprite.replace("{baseUrl}", baseUrl);
+      }
+      if (refStyle.glyphs) {
+        refStyle.glyphs = refStyle.glyphs.replace("{baseUrl}", baseUrl);
+      }
+      console.error('refStyle=' + JSON.stringify(refStyle, null, 2))
+      setStyle(refStyle);
+    }
+
+    initStyle(styleUrl);
+  });
+
+  useEffect(() => {
     if (map.current) return;
-    map.current = new maplibregl.Map({
+    if (!style) return;
+
+    let maplibreMap = new maplibregl.Map({
       container: mapContainer.current as HTMLElement,
-      style: `/style.json`,
+      style: style,
       center: [lng, lat],
       zoom: zoom,
       antialias: true,
@@ -22,40 +67,9 @@ const Map = () => {
       maplibreLogo: true,
       maxBounds: [6.54, 45.77, 7.16, 46.04],
     });
+    map.current = maplibreMap
 
-    // 'building' layer in the streets vector source contains building-height
-    // data from OpenStreetMap.
-    map.current.on("load", function () {
-      //var layers = map.current!.getStyle().layers;
-      map.current!.addLayer(
-        {
-          id: "building-3d",
-          source: "openmaptiles",
-          "source-layer": "building",
-          filter: ["all", ["!has", "hide_3d"]],
-          type: "fill-extrusion",
-          minzoom: 13,
-          layout: { visibility: "visible" },
-          paint: {
-            "fill-extrusion-color": "#aaa",
-            // use an 'interpolate' expression to add a smooth transition effect to the
-            // buildings as the user zooms in
-            "fill-extrusion-height": {
-              property: "render_height",
-              type: "identity",
-            },
-            "fill-extrusion-base": {
-              property: "min_height",
-              type: "identity",
-            },
-            "fill-extrusion-opacity": 0.6,
-          },
-        },
-        "waterway-name"
-      );
-    });
-
-    map.current.addControl(
+    maplibreMap.addControl(
       new maplibregl.NavigationControl({
         visualizePitch: true,
         showZoom: true,
@@ -63,17 +77,61 @@ const Map = () => {
       }),
       "top-right"
     );
-    map.current.addControl(
+    maplibreMap.addControl(
       new maplibregl.TerrainControl({
         source: "terrain_source",
         exaggeration: 1,
       })
     );
-    map.current.addControl(
+    maplibreMap.addControl(
       new maplibregl.ScaleControl({
         maxWidth: 300,
         unit: "metric",
       })
+    );
+
+    // 'building' layer in the openmaptiles vector source contains building-height
+    // data from OpenStreetMap.
+    // FIXME: Add a hillshade layer for hillshade_source
+    maplibreMap.on(
+      "load",
+      () => {
+        console.error("!!!!!!!MAP on load...");
+        map.current!.addLayer(
+          {
+            id: "building-3d",
+            source: "openmaptiles",
+            "source-layer": "building",
+            filter: ["all", ["!has", "hide_3d"]],
+            type: "fill-extrusion",
+            minzoom: 13,
+            layout: { visibility: "visible" },
+            paint: {
+              "fill-extrusion-color": "#aaa",
+              "fill-extrusion-height": {
+                property: "render_height",
+                type: "identity",
+              },
+              "fill-extrusion-base": {
+                property: "min_height",
+                type: "identity",
+              },
+              "fill-extrusion-opacity": 0.6,
+            },
+          },
+          "waterway-name"
+        );
+
+        map.current!.setSky({
+            'sky-color': '#008dff',
+            'sky-horizon-blend': 0.6,
+            'horizon-color': '#88c6f9',
+            'horizon-fog-blend': 0.7,
+            'fog-color': '#c6c6c6',
+            'fog-ground-blend': 0.5 
+        });        
+      },
+      [style]
     );
   });
 
